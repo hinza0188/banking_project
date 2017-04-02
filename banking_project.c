161aaaae0dc42de7d59bcd4eee71994ef942e5e1
Yongki JayZ An <yxa8247@rit.edu>
2017-04-02 18:16:37 -0400

    added git ignore file with example code within the project

--- banking_project.c
+++ banking_project.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
+/*
 int main(int argc, char *argv[]) {
 	printf("Welcome to the QNX Momentics IDE\n");
 	return EXIT_SUCCESS;
 }
+*/
