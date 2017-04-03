14e569088a2fd8fb5f78be7ef0ccdb2fce466ed3
Yongki JayZ An <yxa8247@rit.edu>
2017-04-02 21:21:39 -0400

    now the function properly calculates average time spent for each customer

--- banking_project.c
+++ banking_project.c
@@ -6,9 +6,9 @@
 #include <fifo_q.h>
 
 // define time in nano-second => used in nanosleep();
-#define MIN (100000000L);		// (actual)100ms == (system)1-minute
-#define SEC (100000000L/60);	// (actual)1.67ms == (system)1-second
-#define HR  (100000000L*60);	// (actual)6second == (system)1-hour
+#define MIN (100000000);		// (actual)100ms == (system)1-minute
+#define SEC (100000000/60);	// (actual)1.67ms == (system)1-second
+#define HR  (100000000*60);	// (actual)6second == (system)1-hour
 
 // declare variables
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
